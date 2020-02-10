2c464c5add31a5dff22ccac8dea8fa442bb67ba4
Thomas Wade <wadetc@bc.edu>
2020-02-10 16:28:32 -0500

    class work Bip the Guy

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import AVFoundation
-
+//class
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var audioPlayer = AVAudioPlayer()
